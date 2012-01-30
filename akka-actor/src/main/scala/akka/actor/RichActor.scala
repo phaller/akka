c1fc11edbc5b98dad1974734b14dc83ac40691be
Philipp Haller <hallerp@gmail.com>
2012-01-30 17:34:06 +0100

    Add mailbox initialization. Add stash/unstashAll.

--- akka-actor/src/main/scala/akka/actor/RichActor.scala
+++ akka-actor/src/main/scala/akka/actor/RichActor.scala
@@ -1,10 +1,33 @@
 package akka.actor
 
-import akka.dispatch.{ DispatcherWithStash }
+import akka.dispatch.{ Mailbox, DispatcherWithStash, MessageQueueWithStash }
 import scala.util.continuations.{ cpsParam, shift, reset }
 
 trait RichActor extends Actor {
 
-  override val dispatcher = DispatcherWithStash.globalDispatcher
+  override val dispatcher =
+    DispatcherWithStash.globalDispatcher
+
+  private[this] val mailbox: MessageQueueWithStash = {
+    var mbox: Mailbox = null
+    try {
+      mbox = context.asInstanceOf[ActorCell].mailbox
+    } catch {
+      case e ⇒
+        throw new ActorInitializationException(self, "mailbox with stash required", e)
+    } finally {
+      if (mbox == null || !mbox.isInstanceOf[MessageQueueWithStash])
+        throw new ActorInitializationException(self, "mailbox with stash required")
+    }
+    mbox.asInstanceOf[MessageQueueWithStash]
+  }
+
+  def stash(): Unit = {
+    mailbox.stash(context.currentMessage)
+  }
+
+  def unstashAll(): Unit = {
+    mailbox.unstashAll()
+  }
 
 }
--- akka-actor/src/main/scala/akka/dispatch/DispatcherWithStash.scala
+++ akka-actor/src/main/scala/akka/dispatch/DispatcherWithStash.scala
@@ -4,7 +4,10 @@ import akka.actor.ActorSystem
 
 object DispatcherWithStash {
 
-  lazy val globalDispatcher: MessageDispatcher = {
+  lazy val globalDispatcher: MessageDispatcher =
+    apply()
+
+  def apply(): MessageDispatcher = {
     val sys = ActorSystem()
     sys.dispatcherFactory.newDispatcher("dispatcher-with-mailbox-with-stash", sys.settings.DispatcherThroughput, UnboundedMailboxWithStash()).build
   }
